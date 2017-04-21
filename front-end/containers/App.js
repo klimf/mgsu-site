c1687669b5b508266b794bd5486f51b9658930c0
Никита Яблоков <danger@torman.ru>
2017-04-21 19:15:26 +0300

    sdc

---
+++ front-end/containers/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Du kannst von den Umstanden und Menschen weglaufen, nie kannst du aber von deinen Gedanken und
+                    Gefühlen fliehen.</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
