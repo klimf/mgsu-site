1c97f9e5bfddfd83bab134cbf9e957157d0f7c4b
Клим Фалкенберг <k-f222@ya.ru>
2017-04-26 21:04:07 +0300

    Styles structure refactoring

--- front-end/src/common/components/Header.js
+++ front-end/src/common/components/Header.js
@@ -5,13 +5,15 @@ class Header extends Component {
     render() {
         return (
             <header>
+                <div className="overlay"></div> fixme
                 <NavLink to="/"
                          activeClassName="selected">
                     <div className="logo-white"/>
                 </NavLink>
-                <div className="login-btn-icon-white"/>
-                <div className="donate-btn-white">Сделать вклад</div>
-                <ul className="navigation">
+                <div className="login-btn-icon-white small-0 medium-0"/>
+                <div className="menu-btn-icon-white large-0"/>
+                <div className="donate-btn-white small-0 medium-0">Сделать вклад</div>
+                <ul className="navigation small-0 medium-0">
                     <li className="nav-item">Новости</li>
                     <li className="nav-item">О фонде</li>
                     <li className="nav-item">Контакты</li>
