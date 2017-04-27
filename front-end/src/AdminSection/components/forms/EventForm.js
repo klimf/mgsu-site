f5b5c6cb29c9beae65941b048f4de6b582ab3744
Никита Яблоков <danger@torman.ru>
2017-04-27 20:42:12 +0300

    add basic forms: event, news, people, project

---
+++ front-end/src/AdminSection/components/forms/EventForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Validation from 'react-validation';
+
+export default class extends Component {
+    render() {
+        return <Validation.components.Form>
+            <div>
+                <label>
+                    Название события*
+                    <Validation.components.Input type="text" value='' name="name" validations={['required']}/>
+                </label>
+                <label>
+                    Событие*
+                    <Validation.components.Teaxtarea type="text" value='' name="description" validations={['required']}/>
+                </label>
+
+            </div>
+            <div>
+                <Validation.components.Button>Submit</Validation.components.Button>
+            </div>
+        </Validation.components.Form>;
+    }
+}
