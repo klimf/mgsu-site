85b0503768dc48ec167dae8da39ac5d6dca69d05
Unknown <skyteether@gmail.com>
2017-04-28 21:15:23 +0300

    fixes

--- front-end/src/ProjectsList/ProjectsList.js
+++ front-end/src/ProjectsList/ProjectsList.js
@@ -1,66 +1,46 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ProjectItem from "./components/ProjectItem";
-import {Link, withRouter} from "react-router-dom";
-import {connect, bindActionCreators} from "react-redux";
-import {bindAll} from 'redux-act'
+import { Link, withRouter } from "react-router-dom";
+import { connect, bindActionCreators } from "react-redux";
+import { bindAll } from 'redux-act'
 
-import {GetByDirection, ProjectsListActions as actions} from "./state"
+import { GetByDirection, ProjectsListActions as actions } from "./state"
 
 
 const defaultProps = {
-    projects: [
-        {
-            id: 1,
-            title: "Project title",
-            description: "Description description description description description description description description description description description",
-            image: "placeholder.png"
-        },
-        {
-            id: 2,
-            title: "Project title 2 Project title 2 Project title 2",
-            description: "description",
-            image: "placeholder.png"
-        },
-        {
-            id: 3,
-            title: "Project title 3",
-            description: "description",
-            image: "placeholder.png"
-        },
-        {
-            id: 4,
-            title: "Project title 4",
-            description: "description",
-            image: "placeholder.png"
-        }
 
-    ]
 };
 
 class ProjectsList extends Component {
 
+
     componentDidMount() {
 
-        this.props.changeDirection(this.props.match.params.direction || null);
-        
+        console.log(this.props);
+
+        this.props.getByDirection.perform({
+            query: {
+                direction: this.props.match.params.direction
+            }
+        })
+
     }
-   
- 
+
     render() {
         return (
             <div className="page row expanded">
-                <div className="space-3"/>
+                <div className="space-3" />
                 <div className="content small-12 row">
 
                     {
                         this.props.projects.data &&
                         this.props.projects.data.map((project, index) =>
-                            <ProjectItem key={index} project={project}/>
+                            <ProjectItem key={index} project={project} />
                         )
-                        
+
                     }
                 </div>
-                <div className="space-3"/>
+                <div className="space-3" />
             </div>
         )
     }
@@ -70,14 +50,13 @@ ProjectsList.defaultProps = defaultProps;
 
 const mapStateToProps = (state) => {
     const props = {
-        projects: state.ProjectsList
+        projects: state.ProjectsListAsync
     }
-    return  props;
+    return props;
 }
 
 const mapDispatchToProps = dispatch => ({
-        changeDirection: actions.changeDirection.bindTo(dispatch),
-        getByDirection: bindAll(GetByDirection.actions, dispatch)
-    })
+    getByDirection: GetByDirection.bindTo(dispatch)
+})
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProjectsList));
--- front-end/src/common/helpers.js
+++ front-end/src/common/helpers.js
@@ -1,5 +1,5 @@
 import fetch from "isomorphic-fetch";
-import {createAction} from "redux-act";
+import { createAction, bindAll } from "redux-act";
 
 
 export function getRandomInt(min, max) {
@@ -7,8 +7,8 @@ export function getRandomInt(min, max) {
 }
 
 export function formatMoney(value) {
-    if(value) {
-        return value.toFixed().replace(/./g, function(c, i, a) {
+    if (value) {
+        return value.toFixed().replace(/./g, function (c, i, a) {
             return (i && c !== "." && ((a.length - i) % 3 === 0) ? ' ' + c : c);
         });
     } else {
@@ -18,18 +18,18 @@ export function formatMoney(value) {
 
 export const apiUrl = 'http://185.189.13.148/api/';
 
-export function resolveApi({path, action, query}) {
+export function resolveApi({ path, action, query }) {
 
-    if(query) {
-         var queryArr = [];
+    if (query) {
+        var queryArr = [];
 
 
-        for(var key in query) {
+        for (var key in query) {
             queryArr.push(key + '=' + query[key])
         }
     }
-   
-    
+
+
     return apiUrl + `${path.join('/')}${action ? '/' + action : ''}${query && '/?' + queryArr.join('&&')}`
 
 }
@@ -40,47 +40,46 @@ export class AsyncAction {
     constructor(TYPE, asyncFunc) {
 
         this.actions = {
-            startQuery: createAction(TYPE + '_START'),
+            startQuery: createAction(TYPE + '_START', (params) => params),
             sucessQuery: createAction(TYPE + '_SUCESS', (data) => data),
             failQuery: createAction(TYPE + '_FAIL', (data) => data)
         }
-        
-      
-       
+
         this.asyncFunc = asyncFunc;
         this.reducerHandlers = {
-          [this.actions.startQuery]: (state) => ({loading: true, data: false, error: false}),
-          [this.actions.sucessQuery]: (state, data) => ({loading: false, data: data, error: false}),
-          [this.actions.failQuery]: (state, message) => ({loading: false, data: false, error: message})
+            [this.actions.startQuery]: (state, request) => ({ loading: true, data: false, error: false, request: request }),
+            [this.actions.sucessQuery]: (state, data) => ({ loading: false, data: data, error: false }),
+            [this.actions.failQuery]: (state, message) => ({ loading: false, data: false, error: message })
         };
-        
-        this.defaultState = {loading: true, data: false, error: false}
+
+        this.defaultState = { loading: true, data: false, error: false }
 
     }
 
     perform(params) {
 
-        return (dispatch) => {
+        this.dispatch(this.actions.startQuery(params));
+        this.asyncFunc(params).then(
+            resolved => this.dispatch(this.actions.sucessQuery(resolved)),
+            rejected => this.dispatch(this.actions.failQuery(rejected)))
+    }
 
-            dispatch(this.actions.startQuery())
-            this.asyncFunc(params).then(
-                resolved => dispatch(this.actions.sucessQuery(resolved)),
-                rejected => dispatch(this.actions.failQuery(rejected)))
-        }
-        
+    bindTo(dispatch) {
+        this.dispatch = dispatch;
+        return this;
     }
 }
 
 export class ApiAction extends AsyncAction {
-    
-    constructor({TYPE, model, action, options, prePare}) {
-        
-        const apiFunc = ({params, query, body}) => {
 
-           const path = [this.model].concat(params || []);
-            
-           const apiQuery = resolveApi({path: path, action: this.action || false, query: query || false})
-           
+    constructor({ TYPE, model, action, options, prePare }) {
+
+        const apiFunc = ({ params, query, body }) => {
+
+            const path = [this.model].concat(params || []);
+
+            const apiQuery = resolveApi({ path: path, action: this.action || false, query: query || false })
+
 
             return new Promise((resolve, reject) => {
 
@@ -88,32 +87,32 @@ export class ApiAction extends AsyncAction {
                 options.body = body || null;
 
                 fetch(apiQuery, options).then((response) => {
-                    
+
                     response.json().then((data) => {
 
-                            if(response.status == 200 || response.status == 304) {
-                                resolve(this.prePare(data));
-                            } else {
-                                reject(data);
-                            }
+                        if (response.status == 200 || response.status == 304) {
+                            resolve(this.prePare(data));
+                        } else {
+                            reject(data);
+                        }
 
                     }).catch((response) => {
                         reject(response);
                     })
-                   
+
                 })
 
-            
+
             })
         }
 
         super(TYPE, apiFunc);
 
         this.model = model;
-        this.action= action;
-        this.options = options || {method: 'GET'};
+        this.action = action;
+        this.options = options || { method: 'GET' };
         this.prePare = prePare || ((data) => (data));
-        
+
     }
 
 }
