4cdaeeb6f07476b6af7e518fe5e55f0e30fd0397
Клим Фалкенберг <k-f222@ya.ru>
2017-05-01 21:44:06 +0300

    Add login form

--- front-end/src/common/components/Header.js
+++ front-end/src/common/components/Header.js
@@ -8,6 +8,7 @@ class Header extends Component {
         super(props);
         this.state = {
             menuIsOpen: false,
+            loginIsOpen: false,
             headerIsWhite: this.props.location.pathname === '/'
         }
     }
@@ -16,11 +17,15 @@ class Header extends Component {
         this.setState({menuIsOpen: !this.state.menuIsOpen});
     }
 
+    handlerLoginClick() {
+        this.setState({loginIsOpen: !this.state.loginIsOpen});
+    }
+
     componentDidUpdate() {
 
         this.isAdmin = this.props.user.data && this.props.user.data.role == 1;
         this.isSponsor = this.props.user.data && this.props.user.data.role != 1;
-        this.isNotSign= !this.props.user.data;
+        this.isNotSign = !this.props.user.data;
 
         if (this.state.headerIsWhite !== (this.props.location.pathname === '/')) {
             this.setState({
@@ -32,12 +37,14 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <div className={`overlay large-0" ${this.state.menuIsOpen ? "" : "hide"}`}>
+                <div className={`overlay large-0 " ${this.state.menuIsOpen ? "" : "hide"}`}>
                     <h1 className="inline medium-0 large-0 m-t-1">Меню </h1>
                     <div className="menu-btn-icon right"
                          onClick={this.handlerMenuClick.bind(this)}/>
-                    <button className="overlay-btn" to="">Сделать вклад</button>
-                    <button className="overlay-btn" to="">Войти</button>
+                    <NavLink to="/projects" className="overlay-btn">Сделать вклад</NavLink>
+                    <button className="overlay-btn"
+                            onClick={this.handlerLoginClick.bind(this)}>Войти
+                    </button>
                     <div className="small-navigation no-padding" onClick={this.handlerMenuClick.bind(this)}>
                         <NavLink className="small-nav-item" to="/about">О фонде</NavLink>
                         <NavLink className="small-nav-item" to="/projects">Проекты</NavLink>
@@ -47,27 +54,42 @@ class Header extends Component {
                         <NavLink className="small-nav-item" to="/contacts">Контакты</NavLink>
                     </div>
                 </div>
+                <div className={`overlay large-0" ${this.state.loginIsOpen ? "" : "hide"}`}>
+                    <h1 className="inline medium-0 large-0 m-t-1">Вход </h1>
+                    <div className="back-btn-icon right"
+                         onClick={this.handlerLoginClick.bind(this)}/>
+                    <div className="small-12 space-2 columns"/>
+                    <div className="small-0 small-3 large-4 space-1 columns"/>
+                    <div className="small-12 medium-6 large-4 columns no-padding">
+                        <h2 className="small-0 small-12 uppercase center columns">Вход</h2>
+                        <input className="login-input small-12 columns" type="email" placeholder="Введите почту"/>
+                        <input className="login-input small-12 columns" type="password" placeholder="Введите пароль"/>
+                        <button className="primary-btn h4 small-12 columns">Войти</button>
+                    </div>
+                    <div className="small-0 small-3 large-4 space-1 columns"/>
+                </div>
                 <header>
                     <NavLink to="/"
                              activeClassName="selected">
                         <div className={`logo${this.state.headerIsWhite ? "-white" : ""}`}/>
                     </NavLink>
-                    {this.isAdmin && 
-                     <NavLink className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
-                            to='/admin'/>
+                    {this.isAdmin &&
+                    <NavLink className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
+                             to='/admin'/>
                     }
-                    {this.isSponsor&& 
-                     <NavLink className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
-                            to='/'/>
+                    {this.isSponsor &&
+                    <NavLink className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
+                             to='/'/>
                     }
-                    {this.isNotSign  && 
-                     <NavLink className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
-                            to='/projects'/>
+                    {this.isNotSign &&
+                    <div className={`login-btn-icon${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
+                         onClick={this.handlerLoginClick.bind(this)}/>
                     }
-                   
+
                     <div className={`menu-btn-icon${this.state.headerIsWhite ? "-white" : ""} large-0`}
                          onClick={this.handlerMenuClick.bind(this)}/>
-                    <NavLink className={`donate-btn${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`} to='/projects'>Сделать вклад</NavLink>
+                    <NavLink className={`donate-btn${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}
+                             to='/projects'>Сделать вклад</NavLink>
                     <div className={`navigation${this.state.headerIsWhite ? "-white" : ""} small-0 medium-0`}>
                         <NavLink className="nav-item" to="/about">О фонде</NavLink>
                         <NavLink className="nav-item" to="/projects">Проекты</NavLink>
