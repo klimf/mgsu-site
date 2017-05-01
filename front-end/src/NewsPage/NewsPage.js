becbaa4ea8ec1edaab40553311ce744c12eb18d4
Клим Фалкенберг <k-f222@ya.ru>
2017-05-01 18:12:45 +0300

    Add news page

---
+++ front-end/src/NewsPage/NewsPage.js
@@ -0,0 +1,97 @@
+import React, {Component, propTypes} from "react";
+import {connect, bindActionCreators} from "react-redux";
+
+
+class NewsPage extends Component {
+
+    componentDidMount() {
+
+    }
+
+
+    render() {
+        return (
+            <div className="page row expanded">
+                <div className="small-12 space-3 columns"/>
+                <div className="content small-12 row">
+                    <h1 className="small-12 uppercase center columns">Новости</h1>
+                    <div className="small-12 columns m-b-3">
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+                        <div className="small-12 medium-12 large-6 columns">
+                            <div className="home-news space-6 small-12 columns">
+                                <div className="bg-img"/>
+                                <div className="blackout"/>
+                                <h2>Альбом выпускников</h2>
+                                <p>
+                                    В июле 2017 года будет подготовлен необычный альбом выпускников — CV-book, в котором
+                                    работодатель сможет посмотреть
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
+}
+
+
+export default NewsPage;
+
