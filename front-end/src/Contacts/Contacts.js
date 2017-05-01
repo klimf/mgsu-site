47f7ddd2f32447a2eb40fa9b283183eecc685c89
Клим Фалкенберг <k-f222@ya.ru>
2017-05-01 09:54:26 +0300

    Adaptive contacts page

--- front-end/src/Contacts/Contacts.js
+++ front-end/src/Contacts/Contacts.js
@@ -26,43 +26,43 @@ class ContactsPage extends Component {
                     <h1 className="uppercase center small-12 columns">Наша команда</h1>
                     <div className="small-12 space-2 columns"/>
                     <div className="team-grid small-12 columns">
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Программист</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
-                        <div className="sponsor small-6 medium-4 large-3 columns">
-                            <div className="small-12 sponsor-img columns end"/>
+                        <div className="sponsor small-6 medium-4 large-3 columns end">
+                            <div className="small-12 sponsor-img columns"/>
                             <h2 className="small-12">Паша Техник</h2>
                             <p className="small-12">Спонсор</p>
                         </div>
@@ -70,34 +70,36 @@ class ContactsPage extends Component {
                 </div>
                 <div className="content small-12 row">
                     <div className="small-12 space-3 columns"/>
-                    <div className="huge-text uppercase center small-6 columns">
+                    <div className="huge-text uppercase center small-6 small-0 columns">
                         Карта
                     </div>
-                    <div className="contacts-block small-6 bg-white columns">
-                        <h3 className="uppercase center">Контакты</h3>
-                        <div className="contact">
-                            <p>Адрес</p>
-                            <h3>119049, г. Москва, Ярославское ш., 26</h3>
+                    <div className="contacts-block small-12 medium-6 columns">
+                        <div className="small-12 columns">
+                            <div className="bg-border bg-white"/>
+                            <div className="decoration top-2"/>
+                            <h3 className="uppercase center">Контакты</h3>
+                            <div className="contact">
+                                <p>Адрес</p>
+                                <h3>119049, г. Москва, Ярославское ш., 26</h3>
+                            </div>
+                            <div className="contact">
+                                <p>Телефон</p>
+                                <h3>+7 (495) 781-99-88</h3>
+                            </div>
+                            <div className="contact">
+                                <p>Почта</p>
+                                <h3>ef@mgsu.ru </h3>
+                            </div>
+                            <div className="space-2"/>
                         </div>
-                        <div className="contact">
-                            <p>Телефон</p>
-                            <h3>+7 (495) 781-99-88</h3>
-                        </div>
-                        <div className="contact">
-                            <p>Почта</p>
-                            <h3>ef@mgsu.ru </h3>
-                        </div>
-                        <div className="space-2"/>
-                        <div className="bg-border"/>
-                        <div className="decoration top-2"/>
                     </div>
                 </div>
                 <div className="small-12 expanded">
                     <iframe className="iframe-map"
-                        width="100%"
-                        height="400"
-                        frameBorder="0"
-                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAH0TpAzPZLkOHi2623tYV3Edn-nBCIM2k&q=НИУ+МГСУ">
+                            width="100%"
+                            height="400"
+                            frameBorder="0"
+                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAH0TpAzPZLkOHi2623tYV3Edn-nBCIM2k&q=НИУ+МГСУ">
                     </iframe>
                 </div>
             </div>
