2ddc59d0a63e9ff17c5387e945847ed7d58803b0
Никита Яблоков <danger@torman.ru>
2017-05-02 12:43:08 +0300

    rules

--- front-end/src/AdminSection/validationRules.js
+++ front-end/src/AdminSection/validationRules.js
@@ -7,8 +7,8 @@ import {isEmpty, isEmail, isInt, isAlpha, isLength as length} from "validator";
  * true - warring
  */
 
-export const required = (value = '') => !isEmpty(value) ? undefined : "Обязательное поле";
-export const noSpace = (value = '') => value && value.indexOf(" ") < 0 ? undefined : "Строка не должна содержать пробелов.";
-export const email = (value = '') => value && !isEmail(value) ? undefined : "Не верная почта";
-export const isLength = (min, max) => (value = '') => value && !length(value, {min: min, max: max}) ? `Должно быть больше ${min} символов${max ? ` и меньше ${max}.` : '.'}` : undefined;
-export const onlyEngOrRus = (value = '') => value && (isAlpha(value, ['en-US']) || isAlpha(value, ['ru-RU'])) ? undefined : 'Должны быть только русские или английские буквы';
+export const required = (value = '') => !isEmpty(value) ? false : "Обязательное поле";
+export const noSpace = (value = '') => value && value.indexOf(" ") < 0 ? false : "Строка не должна содержать пробелов.";
+export const email = (value = '') => value && !isEmail(value) ? false : "Не верная почта";
+export const isLength = (min, max) => (value = '') => value && !length(value, {min: min, max: max}) ? `Должно быть больше ${min} символов${max ? ` и меньше ${max}.` : '.'}` : false;
+export const onlyEngOrRus = (value = '') => value && (isAlpha(value, ['en-US']) || isAlpha(value, ['ru-RU'])) ? false : 'Должны быть только русские или английские буквы';
